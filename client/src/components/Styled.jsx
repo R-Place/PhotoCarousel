64a82b3323f5ca3fdd5a3301eecac1ac9d212595
awuradjoaq <awuradjoaq@gmail.com>
2020-10-13 09:59:28 -0700

    added back in styled component header to render styled component on testing

--- client/src/components/Styled.jsx
+++ client/src/components/Styled.jsx
@@ -1,4 +1,4 @@
-//import styled from 'styled-components';
+import styled from 'styled-components';
 
 const PhotoHeader = styled.img`
   font-family: TruliaSans, Roboto, "Segoe UI Bold", Arial, sans-serif;
